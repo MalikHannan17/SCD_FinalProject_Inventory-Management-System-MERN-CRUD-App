af4925a7c030ed79010adc8f14258a5526653ea5
MalikHannan17 <ah2390512@gmail.com>
2025-05-12 22:19:15 +0500

    Initial commit with app and Dockerfile

--- Backend/db.js
+++ Backend/db.js
@@ -1,13 +1,17 @@
-const mongoose = require('mongoose')
-const mongoURI = "mongodb://127.0.0.1:27017/IMS";
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://mongo:27017/IMS"; // Use the Docker service name 'mongo'
 
 const connectToMongo = async () => {
   try {
     mongoose.set("strictQuery", false);
-    mongoose.connect(mongoURI);
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connected to Mongo Successfully!");
   } catch (error) {
-    console.log(error);
+    console.log("Failed to connect to MongoDB:", error);
   }
 };
+
 module.exports = connectToMongo;
